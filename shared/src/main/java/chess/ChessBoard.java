883345d68719651df488463ebd0c91cbbed12b79
dougalcaleb <dougalcaleb@gmail.com>
2025-01-13 18:18:13 -0700

    Implement constructor, get/set methods, and reset for ChessBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,9 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,8 +12,11 @@ package chess;
  */
 public class ChessBoard {
 
-    public ChessBoard() {
-        
+    private ChessPiece[][] board;
+
+    public ChessBoard()
+    {
+        board = new ChessPiece[8][8];
     }
 
     /**
@@ -18,8 +25,9 @@ public class ChessBoard {
      * @param position where to add the piece to
      * @param piece    the piece to add
      */
-    public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+    public void addPiece(ChessPosition position, ChessPiece piece)
+    {
+        board[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -29,15 +37,75 @@ public class ChessBoard {
      * @return Either the piece at the position, or null if no piece is at that
      * position
      */
-    public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+    public ChessPiece getPiece(ChessPosition position)
+    {
+        return board[position.getRow() - 1][position.getColumn() - 1];
     }
 
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
-    public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+    public void resetBoard()
+    {
+        board = new ChessPiece[8][8];
+        setDefaultBoard();
+    }
+
+    private void setDefaultBoard()
+    {
+        // This idea was taken from TestUtilities
+        Character[][] repr = new Character[][] {
+            {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+        };
+
+       Map<Character, ChessPiece.PieceType> charmap = Map.of(
+        'p', ChessPiece.PieceType.PAWN,
+        'n', ChessPiece.PieceType.KNIGHT,
+        'r', ChessPiece.PieceType.ROOK,
+        'q', ChessPiece.PieceType.QUEEN,
+        'k', ChessPiece.PieceType.KING,
+        'b', ChessPiece.PieceType.BISHOP
+       );
+
+        for (int rowIdx = 0; rowIdx < repr.length; rowIdx++)
+        {
+            for (int colIdx = 0; colIdx < repr[rowIdx].length; colIdx++)
+            {
+                Character current = repr[rowIdx][colIdx];
+                ChessPosition pos = new ChessPosition(rowIdx + 1, colIdx + 1);
+                ChessPiece piece = null;
+
+                if (current != ' ') {
+                    piece = new ChessPiece(
+                        Character.isLowerCase(current) ? ChessGame.TeamColor.WHITE : ChessGame.TeamColor.BLACK,
+                        charmap.get(Character.toLowerCase(current))
+                    );
+                }
+
+                addPiece(pos, piece);
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
     }
 }
