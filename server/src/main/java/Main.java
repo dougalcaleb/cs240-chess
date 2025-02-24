e202ab01007ced3e228104223f374cf1edde176d
dougalcaleb <dougalcaleb@gmail.com>
2025-02-24 16:02:18 -0700

    Initial files for phase 3

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,13 @@
-import chess.*;
+import chess.ChessGame;
+import chess.ChessPiece;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        Server webServer = new Server();
+        webServer.run(8080);
     }
 }
