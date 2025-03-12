7a18c5cb1364060e6c5909aaa819ea7aff6ff50d
dougalcaleb <dougalcaleb@gmail.com>
2025-03-12 09:49:48 -0600

    SQL User DAO tests

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -9,10 +9,14 @@ import dataaccess.struct.UserDAO;
 import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.*;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class DataAccessTests {
 
@@ -182,6 +186,143 @@ public class DataAccessTests {
      * UserDAO
      * =========================================================================================
      */
+
+    @Test
+    public void userGetUserReturnsUser()
+    {
+        userdao.setDB(Map.of(
+            "user1", new UserData("user1", "pw1", "email"),
+            "user2", new UserData("user2", "pw2", "email2")
+        ));
+
+        AtomicReference<UserData> data = new AtomicReference<>();
+        Assertions.assertDoesNotThrow(() -> {
+            data.set(userdao.getUser("user2"));
+        });
+
+        Assertions.assertEquals(new UserData("user2", "pw2", "email2"), data.get());
+    }
+
+    @Test
+    public void userGetUserDoesNotReturnNonexistent()
+    {
+        userdao.setDB(Map.of(
+            "user1", new UserData("user1", "pw1", "email"),
+            "user2", new UserData("user2", "pw2", "email2")
+        ));
+
+        Assertions.assertThrows(RuntimeException.class, () -> userdao.getUser("cooluser"));
+    }
+
+    @Test
+    public void userSetUserInsertsUser()
+    {
+        Assertions.assertDoesNotThrow(
+            () -> userdao.setUser(new UserData("user1", "pw1", "email"))
+        );
+        Assertions.assertDoesNotThrow(
+            () -> userdao.getUser("user1")
+        );
+    }
+
+    @Test
+    public void userCannotInsertDuplicateUser()
+    {
+        Assertions.assertDoesNotThrow(
+            () -> userdao.setUser(new UserData("user1", "pw1", "email"))
+        );
+        Assertions.assertThrows(
+            RuntimeException.class,
+            () -> userdao.setUser(new UserData("user1", "pw1", "email"))
+        );
+    }
+
+    @Test
+    public void userUserExistsReturnsUser()
+    {
+        userdao.setDB(Map.of(
+            "user1", new UserData("user1", "pw1", "email"),
+            "user2", new UserData("user2", "pw2", "email2")
+        ));
+
+        boolean result = userdao.userExists(new UserData("user1", "pw1", "email"));
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void userUserDoesNotReturnNonexistent()
+    {
+        userdao.setDB(Map.of(
+            "user1", new UserData("user1", "pw1", "email"),
+            "user2", new UserData("user2", "pw2", "email2")
+        ));
+
+        boolean result = userdao.userExists(new UserData("superuser", "pass", "email"));
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void userGetAllReturnsFullDbList()
+    {
+        userdao.setDB(Map.of(
+            "user1", new UserData("user1", "pw1", "e@mail.com"),
+            "user2", new UserData("user2", "pass", "email@internet.com")
+        ));
+
+        ArrayList<UserData> expected = new ArrayList<>(Arrays.asList(
+                new UserData("user1", "pw1", "e@mail.com"),
+                new UserData("user2", "pass", "email@internet.com")
+        ));
+
+        Assertions.assertIterableEquals(expected, userdao.getAllAsList());
+    }
+
+    @Test
+    public void userGetAllDoesNotReturnNullOrEmpty()
+    {
+        userdao.setDB(Map.of(
+            "user1", new UserData("user1", "pw1", "e@mail.com"),
+            "user2", new UserData("user2", "pass", "email@internet.com")
+        ));
+
+        Collection<UserData> actual = userdao.getAllAsList();
+
+        Assertions.assertNotEquals(null, actual);
+        Assertions.assertEquals(2, actual.size());
+    }
+
+    @Test
+    public void userMatchesUsernamePassword()
+    {
+        userdao.setDB(Map.of(
+            "user1", new UserData("user1", BCrypt.hashpw("pw1", BCrypt.gensalt()), "e@mail.com"),
+            "user2", new UserData("user2", "pass", "email@internet.com")
+        ));
+
+        AtomicBoolean result = new AtomicBoolean();
+        Assertions.assertDoesNotThrow(
+            () -> result.set(userdao.matchUsernamePassword(new UserData("user1", "pw1", "email")))
+        );
+
+        Assertions.assertTrue(result.get());
+    }
+
+    @Test
+    public void userMatchDoesNotMatchIncorrectly()
+    {
+        userdao.setDB(Map.of(
+                "user1", new UserData("user1", BCrypt.hashpw("password", BCrypt.gensalt()), "e@mail.com"),
+                "user2", new UserData("user2", "pass", "email@internet.com")
+        ));
+
+        AtomicBoolean result = new AtomicBoolean();
+        Assertions.assertDoesNotThrow(
+                () -> result.set(userdao.matchUsernamePassword(new UserData("user1", "pw1", "email")))
+        );
+
+        Assertions.assertFalse(result.get());
+    }
+
 
     /**
      * =========================================================================================
